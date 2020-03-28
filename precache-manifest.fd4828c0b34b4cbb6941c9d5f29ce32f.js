e773090b6884d2637d7a919d41e9aa891671a5b6
emryswarlock <ardha275@gmail.com>
2020-03-29 01:16:06 +0700

    Updates

---
+++ precache-manifest.fd4828c0b34b4cbb6941c9d5f29ce32f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3927d3529a23dfe6d602fe94aaf001",
+    "url": "/myrobo/index.html"
+  },
+  {
+    "revision": "5ac629d4309437e6de62",
+    "url": "/myrobo/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "e49ae5bd7e273b4b5f7b",
+    "url": "/myrobo/static/css/main.2491268e.chunk.css"
+  },
+  {
+    "revision": "5ac629d4309437e6de62",
+    "url": "/myrobo/static/js/2.8ca0bda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myrobo/static/js/2.8ca0bda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49ae5bd7e273b4b5f7b",
+    "url": "/myrobo/static/js/main.c8291be5.chunk.js"
+  },
+  {
+    "revision": "1940dea140274e5dfb05",
+    "url": "/myrobo/static/js/runtime-main.5fdaac17.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/myrobo/static/media/SEGA.12a3db8b.woff"
+  }
+]);
